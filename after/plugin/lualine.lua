14e8b5f71e2b6821db4f4905cf1ad0b1bb7c8e7b
Mücahit Tezcan <mmucahit.tezcan@gmail.com>
2024-02-06 14:16:12 +0000

    feat: lualine not knowing path makes me mad

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -2,8 +2,16 @@
 require('lualine').setup({
     options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'auto',
         component_separators = '|',
         section_separators = '',
-    }
+    },
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff'},
+        lualine_c = {{'filename', path = 1}},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'},
+    },
 })
