358ea378b54171700e39f9b4c9eaaaff06fc809c
Mücahit Tezcan <mtezcan@D032-MAC.local>
2024-10-17 03:52:00 +0300

    lazy

--- lua/focusthen/lazy/neotest.lua
+++ lua/focusthen/lazy/neotest.lua
@@ -1,14 +1,14 @@
 return {
     {
         "nvim-neotest/neotest",
-        dependencies = {
+          dependencies = {
+            "nvim-neotest/nvim-nio",
             "nvim-lua/plenary.nvim",
             "antoinemadec/FixCursorHold.nvim",
             "nvim-treesitter/nvim-treesitter",
             "marilari88/neotest-vitest",
             "nvim-neotest/neotest-plenary",
-        },
-        config = function()
+          },config = function()
             local neotest = require("neotest")
             neotest.setup({
                 adapters = {
