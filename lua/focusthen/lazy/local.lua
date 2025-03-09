73660ba2800eeead5ecfe71d4a4a817b7e9d68cc
M. Mücahit Tezcan <mmucahit.tezcan@gmail.com>
2025-03-09 14:50:26 +0000

    feat: making new plugin

--- lua/focusthen/lazy/local.lua
+++ lua/focusthen/lazy/local.lua
@@ -3,6 +3,13 @@ local local_plugins = {
 		"floaterminal",
 		dir = "~/personel/floaterminal.nvim",
 	},
+	{
+		"musicPlayer",
+		dir = "~/personel/music-player.nvim",
+		config = function()
+			require("music-player").setup({})
+		end,
+	},
 }
 
 return local_plugins
