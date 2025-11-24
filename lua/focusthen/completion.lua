226f9ce32f01375d228df1e4f2e1218cbf04b01d
M. Mücahit Tezcan <mmucahit.tezcan@gmail.com>
2025-11-24 20:47:04 +0000

    code action and page up and down

--- lua/focusthen/completion.lua
+++ lua/focusthen/completion.lua
@@ -28,8 +28,8 @@ cmp.setup({
 	completion = { completeopt = "menu,menuone,noinsert" },
 	preselect = cmp.PreselectMode.None,
 	mapping = cmp.mapping.preset.insert({
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-u>"] = cmp.mapping.scroll_docs(-4), -- scroll up
+    ["<C-d>"] = cmp.mapping.scroll_docs(4), -- scroll down
 		["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
 		["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
 		["<C-y>"] = cmp.mapping.confirm({ select = true }),
