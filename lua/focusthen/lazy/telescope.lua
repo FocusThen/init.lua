fae1e732dd17876db0a41598cd300b7f3544786c
M. Mücahit Tezcan <mmucahit.tezcan@gmail.com>
2025-12-21 23:02:28 +0000

    telescope not working

--- lua/focusthen/lazy/telescope.lua
+++ lua/focusthen/lazy/telescope.lua
@@ -1,15 +1,7 @@
 return {
   "nvim-telescope/telescope.nvim",
   cmd = "Telescope",
-  keys = {
-    { "<leader>pf", mode = "n" },
-    { "<C-p>", mode = "n" },
-    { "<leader>pws", mode = "n" },
-    { "<leader>pWs", mode = "n" },
-    { "<leader>ps", mode = "n" },
-    { "<leader>vh", mode = "n" },
-    { "<leader>sb", mode = "n" },
-  },
+  event = "LspAttach",
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
