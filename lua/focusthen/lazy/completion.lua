c2761ca03886cca2ddd4d48d413fc187155be067
M. Mücahit Tezcan <mmucahit.tezcan@gmail.com>
2025-12-21 23:33:43 +0000

    cmp needed

--- lua/focusthen/lazy/completion.lua
+++ lua/focusthen/lazy/completion.lua
@@ -1,18 +1,19 @@
 return {
 	{
 		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
+		lazy = false,
+		priortiy = 100,
 		dependencies = {
 			"onsails/lspkind.nvim",
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-buffer",
-			{ "L3MON4D3/LuaSnip", build = "make install_jsregexp", lazy = true },
+			{ "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
 			"saadparwaiz1/cmp_luasnip",
 			"roobert/tailwindcss-colorizer-cmp.nvim",
 		},
 		config = function()
-      require "focusthen.completion"
+			require("focusthen.completion")
 		end,
 	},
 }
