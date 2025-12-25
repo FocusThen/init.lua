d75669004ea8f97034e404cb8bf6ae9b7c6afbe3
M. Mücahit Tezcan <mmucahit.tezcan@gmail.com>
2025-12-25 03:09:52 +0000

    harpoon2

--- lua/focusthen/lazy/harpoon.lua
+++ lua/focusthen/lazy/harpoon.lua
@@ -1,20 +1,16 @@
 return {
 	"ThePrimeagen/harpoon",
+   branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		local harpoon = require("harpoon")
-		local mark = require("harpoon.mark")
-		local ui = require("harpoon.ui")
 
 		harpoon.setup({})
 
-		vim.keymap.set("n", "<leader>a", mark.add_file)
-		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-		vim.keymap.set("n", "<C-h>", function()
-			ui.nav_next()
-		end)
-		vim.keymap.set("n", "<C-l>", function()
-			ui.nav_prev()
-		end)
+    vim.keymap.set("n", "<C-h>", function() harpoon:list():next() end)
+    vim.keymap.set("n", "<C-l>", function() harpoon:list():prev() end)
 	end,
 }
